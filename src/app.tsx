295a1b3c7f8cdd8a082787ca785fded97665f465
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-02-18 15:39:34 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -40,7 +40,7 @@ export default function App() {
   }
 
   return (
-    <div className="min-w-full min-h-screen p-10 bg-zinc-100 dark:bg-zinc-900 text-black dark:text-white flex">
+    <div className="min-w-full min-h-screen px-6 py-10 bg-zinc-100 dark:bg-zinc-900 text-black dark:text-white flex">
       <div className="flex-grow md:max-w-[800px] mx-auto flex flex-col gap-8 relativ bg-inherit">
         <header className="md:text-center">
           <h1 className="text-4xl font-bold">Tasks</h1>
